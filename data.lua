126ac908f2520c19b273e34816846737cb4ce500
bigyihsuan <bigyihsuan>
2018-04-08 19:15:54 -0400

    forgot to point to the actual files

--- data.lua
+++ data.lua
@@ -1,1 +1,7 @@
-require('prototypes')
+require('prototypes.basic-items')
+require('prototypes.circuits')
+require('prototypes.dyes')
+require('prototypes.military')
+require('prototypes.miscellaneous')
+require('prototypes.recipe-changes')
+require('prototypes.rocket-parts')
